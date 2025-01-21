357578d381d56f41c617e563c2ce62335a91afbd
sumit parmar <79247812+Sumitbittu@users.noreply.github.com>
2025-01-21 14:36:01 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,11 @@
+# 3D Portfolio
+
+🚀 An interactive 3D portfolio built with React.js and animations to showcase my skills, projects, and experiences.
+
+## Features
+
+- **Interactive 3D Experience**: Navigate through a dynamic 3D interface.
+- **Skills & Projects**: Easily explore my technical skills and view projects with detailed descriptions.
+- **Animations**: Smooth transitions and animations to enhance user experience.
+- **Responsive Design**: Optimized for both desktop and mobile devices.
+
---
+++ src/data/constants.js
@@ -0,0 +1,304 @@
+export const Bio = {
+  name: "Sumit Parmar",
+  roles: ["Software Engineer", "Data Analyst", "Java Developer", "Business Analyst"],
+  description:
+    "I'm a Software Developer with over 2+ years of experience in building robust backend systems and scalable web applications. Proficient in Java, Spring Boot, and MySQL, I specialize in API integration, data security, and performance optimization to deliver reliable and efficient solutions.",
+  github: "https://github.com/Sumitbittu",
+  resume:
+    "https://drive.google.com/file/d/1gVV2_mbhQj1ln_ZQbK0qLRFdLD694SQy/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/sumit-parmar-165b801a2/",
+  twitter: "https://www.linkedin.com/in/sumit-parmar-165b801a2/",
+  insta: "https://www.linkedin.com/in/sumit-parmar-165b801a2/",
+  facebook: "https://www.linkedin.com/in/sumit-parmar-165b801a2/",
+};
+
+export const skills = [
+  {
+    title: "Backend Development",
+    skills: [
+      {
+        name: "Java",
+        image:
+          "https://download.logo.wine/logo/Java_(programming_language)/Java_(programming_language)-Logo.wine.png",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "AWS",
+        image:
+          "https://download.logo.wine/logo/Amazon_Web_Services/Amazon_Web_Services-Logo.wine.png",
+      },
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "API Integration",
+        image:
+          "https://www.shutterstock.com/image-vector/api-application-interface-icon-simple-600nw-2188533787.jpg",
+      },
+      {
+        name: "Data Security",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjfH3xWsOsgrD27GJv_1Bza_1fbLz4AIk_0g&s",
+      },
+      {
+        name: "Performance Optimization",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJgxMhCSoNdFagVeph-s38pIJTKYygdsDNfA&s",
+      },
+    ],
+  },
+  {
+    title: "Tools & Platforms",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://e7.pngegg.com/pngimages/713/558/png-clipart-computer-icons-pro-git-github-logo-text-logo-thumbnail.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://static-00.iconduck.com/assets.00/postman-icon-497x512-beb7sy75.png",
+      },
+      {
+        name: "Power PI",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/New_Power_BI_Logo.svg/1200px-New_Power_BI_Logo.svg.png",
+      },
+      {
+        name: "Data Analysis",
+        image:
+          "https://media.istockphoto.com/id/1257312690/vector/analytics-bar-graph-icon.jpg?s=612x612&w=0&k=20&c=3u12q178en00xfxgjwz3xRaTGwrGmWFRdDc3HbJOGHw=",
+      },
+      {
+        name: "Spring Boot",
+        image:
+          "https://www.yessinfotech.com/wp-content/uploads/2019/11/spring-boot-logo.png",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Jupyter",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/Jupyter_logo.svg/1200px-Jupyter_logo.svg.png",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://cdn.iconscout.com/icon/free/png-256/free-mongodb-logo-icon-download-in-svg-png-gif-file-formats--wordmark-programming-langugae-freebies-pack-logos-icons-1175140.png?f=webp",
+      },
+      {
+        name: "Power Apps",
+        image:
+          "https://img.icons8.com/?size=512&id=OU2ddOKw840K&format=png",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqJpgQxCXa_ut0bLFycTpPzoQXnYvgedz1Lg&s",
+    role: "Analyst Software Engineer",
+    company: "Accenture",
+    date: "March 2023 - February 2024",
+    desc: "Led the development of highly scalable backend systems using Java and Python. Managed databases with MySQL and deployed services on AWS for cloud scalability. Integrated APIs and focused on Data Security to ensure safe and efficient data flow. Optimized performance through advanced techniques, significantly improving the system's overall throughput and efficiency.",
+    skills: [
+      "Java",
+      "MySQL",
+      "AWS",
+      "Python",
+      "API Integration",
+      "Data Security",
+      "Performance Optimization"
+    ],
+    doc: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/collections_tool.png?alt=media&token=abcd5678",
+  },
+  {
+    id: 1,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqJpgQxCXa_ut0bLFycTpPzoQXnYvgedz1Lg&s",
+    role: "Associate Software Engineer",
+    company: "Accenture",
+    date: "January 2022 - February 2023",
+    desc: "Developed front-end and back-end components using JavaScript, HTML, CSS, and Spring Boot for web applications. Used Git and GitHub for version control and Postman for API testing. Implemented MongoDB for database management, and utilized Power BI for creating visual analytics dashboards. Delivered robust solutions to ensure seamless user experience and data handling.",
+    skills: [
+      "JavaScript",
+      "HTML",
+      "CSS",
+      "Spring Boot",
+      "Git",
+      "GitHub",
+      "Postman",
+      "MongoDB",
+      "Power BI"
+    ],
+    doc: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/soldout_creations.png?alt=media&token=abcd1121",
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://cdn.theorg.com/70c30331-fa92-43e7-8f85-28118a65aa51_small.jpg",
+    school: "IMS Engineering College, Ghaziabad",
+    date: "July 2017 - June 2021",
+    grade: "8 CGPA",
+    desc: "Completed Bachelor of Technology in Computer Science and Engineering. Courses included Data Structures, Algorithms, OOPs, Database Management, Operating Systems, and Computer Networks. Participated in hackathons and collaborative technical projects.",
+    degree: "B.Tech, Computer Science and Engineering",
+  },
+  {
+    id: 1,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHtrDmeMI342L8i7-0-PycJKaRVAh8nzNFPQ&s",
+    school: "CBSE",
+    date: "July 2015 - June 2017",
+    grade: "8 CGPA",
+    desc: "Completed Class 12 in the Science stream with Computer Science as a specialization.",
+    degree: "Class 12, Science with Computer",
+  },
+  {
+    id: 2,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHtrDmeMI342L8i7-0-PycJKaRVAh8nzNFPQ&s",
+    school: "CBSE",
+    date: "July 2013 - June 2015",
+    grade: "82%",
+    desc: "Completed Class 10 in the Science stream.",
+    degree: "Class 10, Science",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "AI-Image-Generator",
+    date: "Sept 2024",
+    description:
+      "A cutting-edge AI-powered image generator that creates high-quality visuals based on user input. The project leverages advanced machine learning algorithms for image synthesis.",
+    image:
+      "https://www.sourcecodester.com/sites/default/files/images/rems/aig1.png",
+    tags: ["React.js", "Node.js", "Express.js", "AI", "OpenAI API"],
+    category: "AI web app",
+    github: "https://https://github.com/Sumitbittu/AI-Image-Generator",
+    webapp: "https://ai-image-generator-demo.com",
+  },
+  {
+    id: 2,
+    title: "LetsTalk-ChatApp",
+    date: "March 2024",
+    description:
+      "A responsive, real-time chat application with features like user authentication, messaging, and status indicators. Integrated with WebSocket for seamless real-time communication.",
+    image:
+      "https://thumbs.dreamstime.com/b/lets-talk-text-hand-draw-doodle-lettering-vector-134048404.jpg",
+    tags: ["React.js", "Node.js", "MongoDB", "Socket.IO", "AWS"],
+    category: "Web app",
+    github: "https://github.com/Sumitbittu/LetsTalk---Chat-Application",
+    webapp: "https://letstalk-chatapp.com",
+  },
+  {
+    id: 3,
+    title: "GAMES_in_C_Cpp_JAVA",
+    date: "June 2022",
+    description:
+      "A collection of classic games like Tic-Tac-Toe, Snake, and Minesweeper built in C, C++, and Java with interactive UIs and logic implementations.",
+    image:
+      "https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/gigs/268567855/original/d236aca0d76290d1dbb29a4de2848a994a66422c/do-java-or-c-or-cpp-programming-project-or-assignments.png",
+    tags: ["C", "C++", "Java", "Game Development"],
+    category: "Games",
+    github: "https://github.com/Sumitbittu/GAMES_in_C_Cpp_JAVA",
+    webapp: null,
+  },
+  {
+    id: 5,
+    title: "Responsive Web Application Development",
+    date: "June 2022",
+    description:
+      "Contributed to the development of responsive web applications using HTML, CSS, and JavaScript. Applied React JS with hooks for frontend optimization and reusable components, while integrating APIs and implementing error-handling techniques for enhanced reliability. Gained experience with Git and Bitbucket for version control.",
+    image:
+      "https://admin.12grids.com/uploads/blogs/original_cover_images/Webp/Future_Of_Responsive_Web_Design_12Grids.webp",
+    tags: [
+      "React JS",
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "API Integration",
+      "Git",
+      "Bitbucket",
+    ],
+    category: "web app",
+    github: "https://github.com/Sumitbittu/Web-Dev-Projects",
+  },
+  {
+    id: 6,
+    title: "Expense-Tracker",
+    date: "Oct 2022",
+    description:
+      "An intuitive web application that helps users track their income and expenses. Features include monthly reports, budget planning, and data visualization.",
+    image:
+      "https://media.licdn.com/dms/image/D5612AQFQkcIrBJ2O1g/article-cover_image-shrink_720_1280/0/1712583799874?e=2147483647&v=beta&t=JN9z96-7TRw6OfRDiZ43T6QYkcdWZ5jQ3jCqgBhoyaE",
+    tags: ["React.js", "Redux", "Node.js", "MongoDB", "Chart.js"],
+    category: "Web app",
+    github: "https://github.com/Sumitbittu/Expense-Tracker",
+    webapp: "https://expense-tracker-demo.com",
+  },
+  {
+    id: 7,
+    title: "Portfolio-Website",
+    date: "July 2022",
+    description:
+      "A personal portfolio website showcasing my skills, projects, and professional experiences. Developed with React.js and fully responsive for all devices.",
+    image:
+      "https://i.etsystatic.com/12524480/r/il/e4f88f/5854461508/il_570xN.5854461508_q0wd.jpg",
+    tags: ["React.js", "JavaScript", "HTML", "CSS", "Netlify"],
+    category: "Web app",
+    github: "https://github.com/Sumitbittu/Portfolio",
+    webapp: "https://Sumitbittu-portfolio.com",
+  },
+  {
+    id: 8,
+    title: "To-Do-App",
+    date: "March 2022",
+    description:
+      "A simple yet effective to-do list application that allows users to add, edit, and delete tasks. Features include task prioritization and deadline reminders.",
+    image: "https://i.ytimg.com/vi/N2t3IwXvqqo/sddefault.jpg",
+    tags: ["React.js", "Node.js", "MongoDB", "Express.js", "CSS"],
+    category: "Web app",
+    github: "https://github.com/Sumitbittu/Web-Dev-Projects",
+    webapp: "https://todo-app-demo.com",
+  },
+  {
+    id: 9,
+    title: "Weather-App",
+    date: "Jan 2022",
+    description:
+      "A weather forecasting web app that provides real-time weather updates based on location. Integrated with OpenWeatherMap API.",
+    image:
+      "https://s3-alpha.figma.com/hub/file/3863763275/43bb9a44-3863-4ff2-ad24-a2de0a50d129-cover.png",
+    tags: ["React.js", "Node.js", "API Integration", "JavaScript"],
+    category: "Web app",
+    github: "https://github.com/Sumitbittu/Web-Dev-Projects",
+    webapp: "https://weather-app-demo.com",
+  },
+];
